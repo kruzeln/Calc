6c408bfea22576d1e471046a082a3e32b83e1ccb
Zbigniew Jasiak <kruzeln@interia.pl>
2018-06-10 12:24:40 +0200

    kalkulator

--- src/main/java/Calc.java
+++ src/main/java/Calc.java
@@ -1,2 +1,21 @@
 public class Calc {
+    int a;
+    int b;
+
+    public Calc(int a, int b) {
+        this.a = a;
+        this.b = b;
+    }
+    public int sum() {
+        return a + b;
+    }
+
+    public int rem() {
+        return a - b;
+    }
+    public static void main(String args[]) {
+        Calc calc = new Calc(5, 8);
+
+        System.out.println("Sumy: " + (calc.a + calc.b));
+    }
 }
